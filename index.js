75faebf4010cbc973e7f67615ca9fbfb9830177b
NoDenyance <aayaanshbhai@gmail.com>
2025-12-04 18:13:54 -0800

    Update comment for static file serving

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files (cheat scripts, HTML)
+// Serve everything inside /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Default route
